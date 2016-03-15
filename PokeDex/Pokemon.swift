f02853c5427fd0a71a429ae71c3dda50e75967a1
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-15 14:06:37 -0400

    Created the model of a pokemon.

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Emanuel  Guerrero on 3/15/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
