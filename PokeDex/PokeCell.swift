af64bab1e65658db198746643e5481f31a289476
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-15 21:38:51 -0400

    Created a custom collection cell for initializing the cell.

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Emanuel  Guerrero on 3/15/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalizedString
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+}
