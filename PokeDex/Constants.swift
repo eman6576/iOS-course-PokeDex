c91ae98db5c37fa240b5c7de8993d1742fb4fe97
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-17 19:11:58 -0400

    Added a file that contains the urls for the REST endpoints.

---
+++ PokeDex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokeDex
+//
+//  Created by Emanuel  Guerrero on 3/17/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
