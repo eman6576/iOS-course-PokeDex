b6f6acfad3923b979745a0bca275553d4c620a04
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-17 19:10:30 -0400

    Added a method that uses Alamofire to get data from the API.

--- PokeDex/Pokemon.swift
+++ PokeDex/Pokemon.swift
@@ -7,17 +7,19 @@
 //
 
 import Foundation
+import Alamofire
 
 class Pokemon {
     private var _name: String!
     private var _pokedexID: Int!
     private var _description: String!
     private var _type: String!
-    private var _defense: String!
+    private var _defense: Int!
     private var _height: String!
     private var _weight: String!
-    private var _attack: String!
+    private var _attack: Int!
     private var _nextEvolutionText: String!
+    private var _pokemonUrl: String!
     
     var name: String {
         return _name
@@ -30,5 +32,55 @@ class Pokemon {
     init(name: String, pokedexID: Int) {
         self._name = name
         self._pokedexID = pokedexID
+        self._pokemonUrl = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexID)/"
+    }
+    
+    func downloadPokemonDetails(completed: DownloadComplete) {
+        let url = NSURL(string: _pokemonUrl)!
+        
+        Alamofire.request(.GET, url).responseJSON { response in
+            let result = response.result
+            
+            if let dictionary = result.value as? Dictionary<String, AnyObject> {
+                if let weight = dictionary["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dictionary["height"] as? String {
+                    self._height = height
+                }
+                
+                if let attack = dictionary["attack"] as? Int {
+                    self._attack = attack
+                }
+                
+                if let defense = dictionary["defense"] as? Int {
+                    self._defense = defense
+                }
+                
+                print(self._weight)
+                print(self._height)
+                print(self._attack)
+                print(self._defense)
+                
+                if let types = dictionary["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    if let name = types[0]["name"] {
+                        self._type = name.capitalizedString
+                    }
+                    
+                    if types.count > 1 {
+                        for var x = 1; x < types.count; x++ {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name.capitalizedString)"
+                            }
+                        }
+                    }
+                } else {
+                    self._type = ""
+                }
+                
+                print(self._type)
+            }
+        }
     }
 }
