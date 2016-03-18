82106b5055cb573b8a00e9a075b62f2699a596df
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-17 21:27:16 -0400

    Populated the view with the data from the request.

--- PokeDex/PokemonDetailVC.swift
+++ PokeDex/PokemonDetailVC.swift
@@ -27,11 +27,39 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLabel.text = pokemon.name
-        mainImageView.image = UIImage(named: "\(pokemon.pokedexID)")
+        nameLabel.text = pokemon.name.capitalizedString
+        
+        let image = UIImage(named: "\(pokemon.pokedexID)")
+        mainImageView.image = image
+        currentEvoImageView.image = image
         
         pokemon.downloadPokemonDetails { () -> () in
             //This will be called after download is done
+            self.updateUI()
+            
+        }
+    }
+    
+    func updateUI() {
+        descriptionLabel.text = pokemon.description
+        typeLabel.text = pokemon.type
+        defenseLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        pokedexLabel.text = "\(pokemon.pokedexID)"
+        weightLabel.text = pokemon.weight
+        baseAttackLabel.text = pokemon.attack
+        
+        if pokemon.nextEvolutionID == "" {
+            evolutionLabel.text = "No Evolutions"
+            nextEvoImageView.hidden = true
+        } else {
+            nextEvoImageView.hidden = false
+            nextEvoImageView.image = UIImage(named: pokemon.nextEvolutionID)
+            
+            var tempString = "Next Evolution: \(pokemon.nextEvolutionText)"
+            if pokemon.nextEvolutionLevel != "" {
+                tempString += " - LVL \(pokemon.nextEvolutionLevel)"
+            }
         }
     }
 
