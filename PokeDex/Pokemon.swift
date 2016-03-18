9a1ff0bd74ac5d9d1a82c3e4cc92b14d88c1ea8d
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-17 21:26:44 -0400

    Finished parsing the data and cleaned up the print statements from testing.

--- PokeDex/Pokemon.swift
+++ PokeDex/Pokemon.swift
@@ -14,11 +14,13 @@ class Pokemon {
     private var _pokedexID: Int!
     private var _description: String!
     private var _type: String!
-    private var _defense: Int!
+    private var _defense: String!
     private var _height: String!
     private var _weight: String!
-    private var _attack: Int!
+    private var _attack: String!
     private var _nextEvolutionText: String!
+    private var _nextEvolutionID: String!
+    private var _nextEvolutionLevel: String!
     private var _pokemonUrl: String!
     
     var name: String {
@@ -27,6 +29,78 @@ class Pokemon {
     
     var pokedexID: Int {
         return _pokedexID
+    }
+    
+    var description: String {
+        if _description == nil {
+            _description = ""
+        }
+        
+        return _description
+    }
+    
+    var type: String {
+        if _type == nil {
+            _type = ""
+        }
+        
+        return _type
+    }
+    
+    var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
+        
+        return _defense
+    }
+    
+    var height: String {
+        if _height == nil {
+            _height = ""
+        }
+        
+        return _height
+    }
+    
+    var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
+        
+        return _weight
+    }
+    
+    var attack: String {
+        if _attack == nil {
+            _attack = ""
+        }
+        
+        return _attack
+    }
+    
+    var nextEvolutionText: String {
+        if _nextEvolutionText == nil {
+            _nextEvolutionText = ""
+        }
+        
+        return _nextEvolutionText
+    }
+    
+    var nextEvolutionID: String {
+        if _nextEvolutionID == nil {
+            _nextEvolutionID = ""
+        }
+        
+        return _nextEvolutionID
+    }
+    
+    var nextEvolutionLevel: String {
+        if _nextEvolutionLevel == nil {
+            _nextEvolutionLevel = ""
+        }
+        
+        return _nextEvolutionLevel
     }
     
     init(name: String, pokedexID: Int) {
@@ -51,17 +125,12 @@ class Pokemon {
                 }
                 
                 if let attack = dictionary["attack"] as? Int {
-                    self._attack = attack
+                    self._attack = "\(attack)"
                 }
                 
                 if let defense = dictionary["defense"] as? Int {
-                    self._defense = defense
+                    self._defense = "\(defense)"
                 }
-                
-                print(self._weight)
-                print(self._height)
-                print(self._attack)
-                print(self._defense)
                 
                 if let types = dictionary["types"] as? [Dictionary<String, String>] where types.count > 0 {
                     if let name = types[0]["name"] {
@@ -79,7 +148,44 @@ class Pokemon {
                     self._type = ""
                 }
                 
-                print(self._type)
+                if let descriptionArray = dictionary["descriptions"] as? [Dictionary<String, String>] where descriptionArray.count > 0 {
+                    if let url = descriptionArray[0]["resource_uri"] {
+                        let nsURL = NSURL(string: "\(URL_BASE)\(url)")!
+                        
+                        Alamofire.request(.GET, nsURL).responseJSON { response in
+                            let descriptionResult = response.result
+                            if let descriptionDictionary = descriptionResult.value as? Dictionary<String, AnyObject> {
+                                if let description = descriptionDictionary["description"] as? String {
+                                    self._description = description
+                                    print(self._description)
+                                }
+                            }
+                            completed()
+                        }
+                    }
+                } else {
+                    self._description = ""
+                }
+                
+                if let evolutions = dictionary["evolutions"] as? [Dictionary<String, AnyObject>] where evolutions.count > 0 {
+                    if let to = evolutions[0]["to"] as? String {
+                        //Can't support mega pokemon right now but
+                        //API still has mega data
+                        if to.rangeOfString("mega") == nil {
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                let newString = uri.stringByReplacingOccurrencesOfString("/api/v1/pokemon/", withString: "")
+                                let number = newString.stringByReplacingOccurrencesOfString("/", withString: "")
+                                
+                                self._nextEvolutionID = number
+                                self._nextEvolutionText = to
+                                
+                                if let level = evolutions[0]["level"] as? Int {
+                                    self._nextEvolutionLevel = "\(level)"
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
